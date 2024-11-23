2152989b426f7b3d337e3175b709f3e19c593944
saleemkheralden <saleemkheralden@gmail.com>
2024-11-23 16:25:11 +0200

    API for stonks

---
+++ models.py
@@ -0,0 +1,23 @@
+import torch 
+from torch import nn
+
+class Transformer(nn.Module):
+	def __init__(self):
+		pass
+
+	def forward(self, x):
+		return x
+
+
+
+
+
+
+# def train_model(model, dataloader):
+
+	
+
+
+
+
+
